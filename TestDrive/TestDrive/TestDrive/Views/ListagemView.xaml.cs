7adece7dabbc0d96fdb67b5cc9ec4e0355fd6acb
Tiago Fusco <tiagofusco@outlook.com>
2017-09-05 21:52:35 -0300

    Atualização testDrive

---
+++ TestDrive/TestDrive/TestDrive/Views/ListagemView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace TestDrive.Views
+{
+    public class Veiculo
+    {
+        public string nome { get; set; }
+        public decimal preco { get; set; }
+        public string PrecoFormatado
+        {
+            get
+            {
+                return string.Format("R$ {0}", preco);
+            }
+        }
+
+        public FormattedString VeiculoLabel
+        {
+            get
+            {
+                return new FormattedString
+                {
+                    Spans =
+                    {
+                        new Span {Text = nome },
+                        new Span {Text = " - " },
+                        new Span {Text = PrecoFormatado, FontAttributes.Bold } }
+                };
+            }
+            set { }
+        }
+    }
+
+
+        public partial class ListagemView : ContentPage
+        {
+            public List<Veiculo> Veiculos { get; set; }
+        }
+            public ListagemView()
+            {
+               InitializeComponent();
+
+               this.Veiculos = new List<Veiculo>()
+               {
+                   new Veiculo {nome= "Azera v6", preco = 85000 },
+               }
+
+
+            }
+   
+
+    
+}
+
+
